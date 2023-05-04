6c9519acea7e027188ab1c95aa04b102e31a7662
Baumel Márton <marcibaumel@gmail.com>
2023-05-04 18:33:20 +0200

    init

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.app {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+        
+    opens com.example.app to javafx.fxml;
+    exports com.example.app;
+}
