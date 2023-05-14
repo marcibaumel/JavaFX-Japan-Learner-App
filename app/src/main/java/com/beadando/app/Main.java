8ce6fb12e64d58fce6393895109a6a46805f24f6
Baumel Márton <marcibaumel@gmail.com>
2023-05-14 09:18:12 +0200

    Icon

--- app/src/main/java/com/beadando/app/Main.java
+++ app/src/main/java/com/beadando/app/Main.java
@@ -3,6 +3,7 @@ package com.beadando.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Welcome.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Image icon = new Image(getClass().getResource("/images/icon.png").toString());
+        stage.getIcons().add(icon);
         stage.setTitle("Quiz App");
         stage.setScene(scene);
         stage.show();
